c026dbf890b52a4d5206b8d503f25112368184c6
Pavao Piršljin <pavaopirsljin@gmail.com>
2018-07-04 13:35:52 +0200

    started todo list

---
+++ todolist/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>TodoList</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+        
+    </div>
+    <footer id="footer">
+        <p class="text-center">Copyright &copy; 2018 TodoList</p>
+    </footer>
+
+</body>
+</html>
